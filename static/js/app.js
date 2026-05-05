f5ebff5c5f75bb44eb730cd20c092fe6dcfa44ae
Дмитрий Вишневский <mr.dimitriy9888@mail.ru>
2026-05-05 15:59:55 +0300

    AddGroupedRowAndFuctional

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,9 @@
 import { AuthModal } from './components/AuthModal.js';
 import { TasksModal } from './components/TasksModal.js';
 import { TaskViewModal } from './components/TaskViewModal.js';
+import { GroupModal } from './components/GroupModal.js';
 
+import { TasksTable } from './components/TaskTable.js';
 import { TaskWheel } from './components/TaskWheel.js';
 
 import { AuthService } from './services/AuthService.js';
@@ -15,8 +17,11 @@ class Program {
         this.authModal = null;
 
         this.authService = new AuthService();
+        this.tasksTable= new TasksTable();
         this.tasksModal = new TasksModal();
+        this.groupModal = new GroupModal();
         this.taskWheel = new TaskWheel();
+        
         this.taskViewModal = null;
     }
 
@@ -29,7 +34,7 @@ class Program {
             } else {
                 console.log('Auto-login failed:', autoLoginResult.error);
             }
-
+            
             this.initializeUI();
             this.isInitialized = true;
             console.log('App initialized successfully');
@@ -38,20 +43,30 @@ class Program {
         }
     }
 
-    initializeUI() {
+    async initializeUI() {
         this.authModal = new AuthModal(
             this.authService,
             (user) => this.onAuthSuccess(user)
         );
+        this.sidebarToggleInit();
         this.authModal.initialize();
+        this.tasksTable.initialize();
         this.tasksModal.initialize();
-        this.taskViewModal = new TaskViewModal(this.tasksModal);
+        this.groupModal.initialize();
+        this.taskViewModal = new TaskViewModal(this.tasksModal,this.groupModal );
         this.taskViewModal.initialize();
 
         this.taskWheel.initialize();
 
         this.bindButtons();
         this.bindEvents();
+
+        await this.groupModal.uppdateGroupCash();
+
+        const selectGroup = document.getElementById('groupFilter');
+        selectGroup.innerHTML = '<option value="" disabled selected>Выберите группу</option>'
+            + this.groupModal.groupsCash.map(g => `<option value="${g.group_id}">${g.group_name}</option>`).join('');
+
     }
 
     //Привязываем действия к кнопкам 
@@ -67,7 +82,7 @@ class Program {
         document.getElementById('uppdateTaskBtn').addEventListener('click', () => {
             const filtr = document.getElementById('statusFilter');
             const statusText = filtr.value == '' ? '': `status=${filtr.value}`;
-            this.tasksModal.fetchData(true, statusText);
+            this.tasksTable.fetchData(true, statusText);
         });
 
         document.getElementById('randomTaskBtn').addEventListener('click', async () => {
@@ -77,7 +92,7 @@ class Program {
         document.getElementById("my_task").addEventListener('change', async () => {
             const filtr = document.getElementById('statusFilter');
             const statusText = filtr.value == '' ? '': `status=${filtr.value}`;
-            this.tasksModal.fetchData(true, statusText);
+            this.tasksTable.fetchData(true, statusText);
         });
     }
 
@@ -89,12 +104,34 @@ class Program {
 
         // Обновление таблицы после удаления
         document.addEventListener('task:deleted', () => {
-            this.tasksModal.fetchData();
+            this.tasksTable.fetchData();
         });
 
         document.getElementById('statusFilter').addEventListener('change',(e)=>{
-            this.tasksModal.fetchData(true, e.target.value != ''? `status=${e.target.value}`: '');
+            this.tasksTable.fetchData(true, e.target.value != ''? `status=${e.target.value}`: '');
         });
+    }
+
+
+    sidebarToggleInit() {
+        const sidebar = document.getElementById('sidebar');
+        const sidebarToggle = document.getElementById('sidebarToggle');
+        
+        if (sidebarToggle && sidebar) {
+            sidebarToggle.addEventListener('click', function() {
+                sidebar.classList.toggle('collapsed');
+                
+                // Сохраняем состояние в localStorage (опционально)
+                const isCollapsed = sidebar.classList.contains('collapsed');
+                localStorage.setItem('sidebarCollapsed', isCollapsed);
+            });
+            
+            // Восстанавливаем состояние при загрузке
+            const isCollapsed = localStorage.getItem('sidebarCollapsed') === 'true';
+            if (isCollapsed) {
+                sidebar.classList.add('collapsed');
+            }
+        }
     }
 
     async logout() {
@@ -104,7 +141,7 @@ class Program {
             document.getElementById('quit').hidden = true;
             document.getElementById('regLog').hidden = false;
             console.log('User logged out');
-            await this.tasksModal.fetchData();
+            await this.tasksTable.fetchData();
         }
     }
 
@@ -125,7 +162,7 @@ class Program {
         document.getElementById('quit').hidden = false;
         document.getElementById('regLog').hidden = true;
 
-        await this.tasksModal.fetchData();
+        await this.tasksTable.fetchData();
     }
 }
 
@@ -135,3 +172,4 @@ const app = new Program();
 document.addEventListener('DOMContentLoaded', () => {
     app.initialize();
 });
+
