d11b3b7ecf1dd14073ccd382a076edef76c81095
Daniil Yakimtsev <dan.yakimtztff@yandex.ru>
2026-05-06 08:50:56 +0300

    refactor: change group dto

--- internal/model/dto/task_group_dto.go
+++ internal/model/dto/task_group_dto.go
@@ -3,8 +3,8 @@ package dto
 import "time"
 
 type TaskGroupDTO struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description,omitempty"`
+	ID          int       `json:"group_id"`
+	Name        string    `json:"group_name"`
+	Description string    `json:"group_desc,omitempty"`
 	CreatedAt   time.Time `json:"created_at"`
 }
